8ef9c4cf13ce6eaa6a73a2dc19e11d18200bddac
Bharat Kalluri <bharatkalluri@protonmail.com>
2020-12-18 16:25:00 +0530

    Adding cluster support

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ErrorsInterceptor } from './shared/interceptors/errors.interceptor';
 import { ConfigService } from '@nestjs/config';
 import { PinoLogger } from 'nestjs-pino';
+import * as cluster from 'cluster';
+import * as os from 'os';
 
 const APP_NAME = 'Core ' + process.env.NODE_ENV;
 
@@ -20,36 +22,51 @@ async function swaggerModule(app: INestApplication) {
   SwaggerModule.setup('swagger', app, document);
 }
 
-async function startServer() {
-  const app = await NestFactory.create(AppModule);
-  const configService: ConfigService = app.get(ConfigService);
-  const logger: PinoLogger = await app.resolve(PinoLogger);
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
 
-  // Middleware
-  // TODO: Change these as per project and scale Rate limiting: limit each IP to 100 requests per 15 minutes
-  // TODO: Need to plan something else here, as it will not work properly with multiple processes.
-  logger.debug('Loading middleware');
-  app.use(
-    rateLimit({
-      windowMs: configService.get<number>('RATE_LIMIT_WINDOW_MS'),
-      max: configService.get<number>('RATE_LIMIT_COUNT'),
-      message: `Too many accounts created from this IP, please try again after ${
-        (configService.get<number>('RATE_LIMIT_WINDOW_MS') as number) / 60000
-      } minutes`,
-    }),
-  );
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
-  //Swagger setup
-  swaggerModule(app);
+  cluster.on('online', (worker) => {
+    console.log('Worker ' + worker.process.pid + ' is online.');
+  });
+  cluster.on('exit', ({ process }) => {
+    console.log('worker ' + process.pid + ' died.');
+  });
+} else {
+  async function startServer() {
+    const app = await NestFactory.create(AppModule);
+    const configService: ConfigService = app.get(ConfigService);
+    const logger: PinoLogger = await app.resolve(PinoLogger);
 
-  // Global validation pipe based on DTO class, also transforms payload to DTO object
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    logger.debug('Loading middleware');
+    // Middleware
+    // TODO: Change these as per project and scale Rate limiting: limit each IP to 100 requests per 15 minutes
+    // TODO: Need to plan something else here, as it will not work properly with multiple processes.
+    app.use(
+      rateLimit({
+        windowMs: configService.get<number>('RATE_LIMIT_WINDOW_MS'),
+        max: configService.get<number>('RATE_LIMIT_COUNT'),
+        message: `Too many accounts created from this IP, please try again after ${
+          (configService.get<number>('RATE_LIMIT_WINDOW_MS') as number) / 60000
+        } minutes`,
+      }),
+    );
 
-  //Global Error Interceptor
-  app.useGlobalInterceptors(new ErrorsInterceptor());
+    //Swagger setup
+    swaggerModule(app);
 
-  logger.debug('Started Listening for Server Port');
-  await app.listen(8080);
+    // Global validation pipe based on DTO class, also transforms payload to DTO object
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+    //Global Error Interceptor
+    app.useGlobalInterceptors(new ErrorsInterceptor());
+
+    logger.debug('Started Listening for Server Port');
+    await app.listen(8080);
+  }
+
+  startServer();
 }
-
-startServer();
